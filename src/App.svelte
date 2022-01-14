4359a0e1a88cc6338e98d85bd8f7de6dc309b5b9
lmuzzupappa <lmuzzupappa@glamit.com.ar>
2022-01-14 18:08:25 -0300

    form created

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let name;
+
+  import TodoForm from "./TodoForm.svelte";
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <TodoForm />
+</main>
+
+<style>
+</style>
---
+++ src/TodoForm.svelte
@@ -0,0 +1,15 @@
+<script>
+  function formSubmitted(event) {
+    event.preventDefault();
+
+    console.log("Form was submitted");
+  }
+</script>
+
+<form on:submit|preventDefault={formSubmitted}>
+  <label>
+    New Todo
+    <input type="text" placeholder="Enter a todo ..." />
+  </label>
+  <button>Add Todo</button>
+</form>
